539396ff53e5814d0c69dbe3cf0516d36ad11a37
simonlimon <simon.posada29@gmail.com>
2017-11-24 23:45:23 +0100

    Initial Okta integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import Home from './Home';
+
+const config = {
+  issuer: 'https://dev-894240-admin.oktapreview.com/oauth2/default',
+  redirectUri: window.location.origin + '/implicit/callback',
+  clientId: '0oacycxjwzsnZ8CKp0h7'
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Security
+          issuer={config.issuer}
+          client_id={config.clientId}
+          redirect_uri={config.redirect_uri}
+        >
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/implicit/callback" component={ImplicitCallback} />
+        </Security>
+      </Router>
     );
   }
 }
