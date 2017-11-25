e79f3d52b890349bb10f019ad77b1578f8c3122b
simonlimon <simon.posada29@gmail.com>
2017-11-25 01:09:54 +0100

    Update routing

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import http from 'http';
 import socketio from 'socket.io';
 import router from './router';
@@ -11,9 +12,15 @@ app.set('port', process.env.PORT || 3001);
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
 }
 
+// API Routing is handled externally
 app.use('/api', router);
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+});
 
 app.listen(app.get('port'));
