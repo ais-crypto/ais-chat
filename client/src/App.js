0ab130dcdc339cb358897233d36a3663d74a5a1d
simonlimon <simon.posada29@gmail.com>
2017-11-25 10:51:41 +0100

    Authentication routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
-import Home from './Home';
+import Home from './components/Home.react';
+import Login from './components/auth/Login.react';
+import Protected from './components/Protected.react';
+
+function onAuthRequired({ history }) {
+  history.push('/login');
+}
 
 const config = {
-  issuer: 'https://dev-894240-admin.oktapreview.com/oauth2/default',
+  baseUrl: 'https://dev-894240.oktapreview.com/',
   redirectUri: window.location.origin + '/implicit/callback',
   clientId: '0oacycxjwzsnZ8CKp0h7'
 };
 
 class App extends Component {
   render() {
+    console.log(config.redirectUri);
     return (
       <Router>
         <Security
-          issuer={config.issuer}
+          issuer={config.baseUrl + 'oauth2/default'}
           client_id={config.clientId}
-          redirect_uri={config.redirect_uri}
+          redirect_uri={config.redirectUri}
+          onAuthRequired={onAuthRequired}
         >
           <Route path="/" exact={true} component={Home} />
+          <SecureRoute path="/protected" component={Protected} />
+          <Route
+            path="/login"
+            render={() => <Login baseUrl={config.baseUrl} />}
+          />
           <Route path="/implicit/callback" component={ImplicitCallback} />
         </Security>
       </Router>
