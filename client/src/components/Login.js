331a092533e7d7e568d3448263e7087555fd60d3
simonlimon <simonpfish@gmail.com>
2017-11-30 13:00:21 +0100

    Simple login button for testing

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+const authOrigin =
+  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3001';
 
 class Login extends Component {
   render() {
-    return <div />;
+    return <a href={authOrigin + '/auth/google'}>Sign in with Google</a>;
   }
 }
 
