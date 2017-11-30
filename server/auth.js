0f14ab204ea93cde2b996fd5eb3aa7786432c5f6
simonlimon <simonpfish@gmail.com>
2017-11-30 11:29:21 +0100

    Auth backend setup

--- server/auth.js
+++ server/auth.js
@@ -1,9 +1,43 @@
 import { Router } from 'express';
+import passport from 'passport';
+import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
 
 const router = Router();
+const callbackOrigin =
+  process.env.NODE_ENV === 'production'
+    ? 'https://ais-chat.herokuapp.com'
+    : 'http://localhost:3001';
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the AIS chat API!' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: callbackOrigin + '/auth/google/callback'
+    },
+    function(accessToken, refreshToken, profile, done) {
+      console.log(profile);
+      return done();
+    }
+  )
+);
+
+// ------------------- ROUTES -------------------
+
+router.get('/google', passport.authenticate('google', { scope: 'profile' }));
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+// Middleware for authentication checks
+router.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/');
+};
 
 export default router;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import path from 'path';
 import http from 'http';
+import passport from 'passport';
 import socketio from 'socket.io';
 import router from './router';
-import auth form './auth'
+import auth from './auth';
 
 const app = express();
 const server = http.createServer(app);
 const io = new socketio(server);
 
 app.set('port', process.env.PORT || 3001);
+
+// Auth middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // API Routing is handled externally
 app.use('/api', router);
