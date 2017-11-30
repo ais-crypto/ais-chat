55b4e39a29d2a77099962994dbdf98d17dd0fc99
simonlimon <simonpfish@gmail.com>
2017-11-30 10:16:07 +0100

    Correct api priority on express

--- server/index.js
+++ server/index.js
@@ -10,26 +10,21 @@ const io = new socketio(server);
 
 app.set('port', process.env.PORT || 3001);
 
+// API Routing is handled externally
+app.use('/api', router);
+
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, '../client/build')));
 }
 
-// API Routing is handled externally
-app.use('/api', router);
-
-// All remaining requests return the React app, so it can handle routing.
-app.get('*', (request, response) => {
-  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-
 server.listen(app.get('port'));
 
 console.log(`Listening on: ${app.get('port')}`);
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log(`a user connected`);
   socket.on('disconnect', () => {
-    console.log(`a user has disconnected`)
+    console.log(`a user has disconnected`);
   });
 });
