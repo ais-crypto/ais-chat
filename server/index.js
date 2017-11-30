224aed86a411b0da74f6e0fb11aa7a60a1387102
simonlimon <simonpfish@gmail.com>
2017-11-30 12:23:43 +0100

    correct react-router behaviour

--- server/index.js
+++ server/index.js
@@ -13,10 +13,10 @@ app.set('port', process.env.PORT || 3001);
 // API Routing is handled externally
 app.use('/api', router);
 
-// Express only serves static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '../client/build')));
-}
+// All remaining requests return the React app, so it can handle routing
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 server.listen(app.get('port'));
 
