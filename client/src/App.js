a8b35a7966629db1b121cd6ac4fe66c9c225d093
simonlimon <simonpfish@gmail.com>
2017-12-03 22:15:25 +0100

    dark ui theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Home from './components/Home';
 import Chat from './components/Chat';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/:chatname" component={Chat} />
-        </Switch>
-      </Router>
+      <div style={{ backgroundColor: 'rgb(48,48,48)' }}>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/:chatname" component={Chat} />
+            </Switch>
+          </Router>
+        </MuiThemeProvider>
+      </div>
     );
   }
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,32 +1,29 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Row, Col } from 'react-flexbox-grid';
 
 class Home extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <Row middle="xs" center="xs" style={{ height: window.innerHeight }}>
-          <Col>
-            <form
-              onSubmit={e => {
-                this.props.history.push(this.state.room);
-                e.preventDefault();
+      <Row middle="xs" center="xs" style={{ height: window.innerHeight }}>
+        <Col>
+          <form
+            onSubmit={e => {
+              this.props.history.push(this.state.room);
+              e.preventDefault();
+            }}
+          >
+            <TextField
+              className="room-input"
+              hintText="Type a room name"
+              floatingLabelText="Create or join a room"
+              onChange={e => {
+                this.setState({ room: e.target.value });
               }}
-            >
-              <TextField
-                className="room-input"
-                hintText="Type a room name"
-                floatingLabelText="Create or join a room"
-                onChange={e => {
-                  this.setState({ room: e.target.value });
-                }}
-              />
-            </form>
-          </Col>
-        </Row>
-      </MuiThemeProvider>
+            />
+          </form>
+        </Col>
+      </Row>
     );
   }
 }
