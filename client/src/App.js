2fad7fcd23baa6dd5c22725f52a187da7a255b02
annie ke <annieke8@gmail.com>
2017-12-05 14:11:03 +0100

    more eslint changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -6,23 +6,21 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Home from './components/Home';
 import Chat from './components/Chat';
 
-class App extends Component {
-  render() {
-    return (
-      <div
-        style={{ backgroundColor: 'rgb(48,48,48)', height: window.innerHeight }}
-      >
-        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/:chatname" component={Chat} />
-            </Switch>
-          </Router>
-        </MuiThemeProvider>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div
+      style={{ backgroundColor: 'rgb(48,48,48)', height: window.innerHeight }}
+    >
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/:chatname" component={Chat} />
+          </Switch>
+        </Router>
+      </MuiThemeProvider>
+    </div>
+  );
+};
 
 export default App;
